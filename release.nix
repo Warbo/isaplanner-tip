e8600e3677e0571973342ad7e52b5c064bc9310f
Chris Warburton <chriswarbo@gmail.com>
2016-07-07 15:48:08 +0100

    Added release.nix

---
+++ release.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+
+{
+  isaplannerDocker = stdenv.mkDerivation {
+    name = "isaplanner";
+    src  = fetchFromGitHub {
+      owner  = "TheoryMine";
+      repo   = "IsaPlanner";
+      rev    = "f3f4220";
+      sha256 = "xkzxfdnqnx9p43r40vjwskklqn0mqxg6";
+    };
+
+
+    buildPhase = ''
+      docker build -t theorymine/isaplanner:2015.0.2 .
+    '';
+
+    installPhase = ''
+      mkdir -p "$out/bin"
+      cp "${isaplannerRun}" "$out/bin/isaplanner-run"
+    '';
+
+    isaplannerRun = writeFile "isaplanner-run" ''
+      docker run -i -t theorymine/isaplanner:2015.0.2 /bin/bash
+    '';
+  };
+}
