945f5034c8de95cbf13bacdd1281986b85cd9007
Chris Warburton <chriswarbo@gmail.com>
2016-08-16 14:57:10 +0100

    Add working isacosy script

--- default.nix
+++ default.nix
@@ -136,22 +136,71 @@ with pkgs; rec {
             echo 'use_thy "Invoke";' | isabelle console
           '';
         };
+
+  isacosy-nat =
+    let theory = writeScript "IsaCoSyNat.thy" ''
+          theory Invoke
+          imports Main IsaP IsaCoSy Orderings Set Pure List
+          begin
+
+          ML {*
+          val datatypes = [@{typ "nat"}, @{typ "nat list"}];
+          val functions = map Term.dest_Const
+            [@{term "Groups.plus_class.plus :: nat ? nat ? nat"},
+            @{term "Groups.minus_class.minus :: nat ? nat ? nat"},
+            @{term "Groups.times_class.times :: nat ? nat ? nat"},
+            @{term "List.append :: nat list ? nat list ? nat list"}];
+          val def_thrms = [@{thms "Nat.plus_nat.simps"}, @{thms "Nat.minus_nat.simps"},  @{thms
+          "Nat.times_nat.simps"},
+                       @{thms "List.append.simps"}];
+          val thrms = flat def_thrms;
+          val fundefs = functions ~~ def_thrms;
+          (* Don't want to synthesise undefined terms *)
+          val constr_trms = [Trm.change_frees_to_fresh_vars @{term "hd([])"},
+                             Trm.change_frees_to_fresh_vars @{term "tl([])"}
+            ];
+
+          (* set constraint params *)
+            val cparams0 =
+                ConstraintParams.empty
+                  |> ThyConstraintParams.add_eq @{context}
+                  |> ThyConstraintParams.add_datatype' @{context} @{typ "nat"}
+                  |> ThyConstraintParams.add_datatype' @{context} @{typ "nat list"}
+                  |> (ConstraintParams.add_consts functions)
+                  |> ConstraintParams.add_arb_terms @{context} constr_trms
+                  (*|> ConstraintParams.add_thms @{context} thrms*);
+          val (init_ctxt, cparams) =
+            ConstraintParams.add_ac_properties_of_consts @{context} fundefs cparams0;
+           ConstraintParams.print init_ctxt cparams;
+
+          val thy_constraints = (Constraints.init init_ctxt cparams);
+          val top_term = Thm.term_of @{cpat "op = :: ?'a => ?'a => bool"};
+          val top_const = (Constant.mk (fst (Term.dest_Const top_term)));
+          val dummy_prover = SynthInterface.Prover(fn ctxt => fn term => (NONE,[]));
+
+          val (nw_cparams, nw_ctxt) = SynthInterface.thm_synth
+            SynthInterface.rippling_prover
+            SynthInterface.quickcheck
+            SynthInterface.wave_rule_config
+            SynthInterface.var_allowed_in_lhs
+            {max_size = 8, min_size = 3, max_vars = 3, max_nesting= SOME 2}
+            (Constant.mk "HOL.eq") (cparams0,@{context});
+          (*map (Trm.print nw_ctxt) (SynthOutput.get_all (SynthOutput.Ctxt.get nw_ctxt));*)
+
+          map (Trm.print nw_ctxt) (SynthOutput.get_conjs (SynthOutput.Ctxt.get nw_ctxt));
+          *}
+
+          end
+        '';
+    in stdenv.mkDerivation {
+         name = "isacosy-nat";
+         buildInputs  = [ isaplanner ];
+         inherit isaplanner;
+         buildPhase   = ''
+           # Theory name must match file name; 'tip' uses the name "A"
+           cp "$theory" "IsaCoSyNat.thy"
+
+           echo 'use_thy "IsaCoSy";' | isabelle -l HOL-IsaPlannerSession -d "$isaplanner/contrib/IsaPlanner" console
+         '';
+  };
 }
-
-# # Install the necessary base packages for Isabelle/TheoryMine theorem
-# # generation
-# RUN apt-get update && apt-get install -y \
-#   g++ \
-#   git \
-#   make \
-#   openjdk-6-jre \
-#   subversion \
-#   wget \
-#   nano
-
-# # Install additional packages for latex/certifictae/image generation
-# RUN apt-get install -y \
-#   imagemagick \
-#   curl \
-#   texlive-latex-extra \
-#   latex-cjk-all
