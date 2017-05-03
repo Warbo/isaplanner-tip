0b2ffc130173c9b4eabd8b3f978a3047060a0fb3
Chris Warburton <chriswarbo@gmail.com>
2017-05-03 18:47:13 +0100

    Strip out non-derivations

--- release.nix
+++ release.nix
@@ -8,11 +8,13 @@ with rec {
 
   # Remove 'debug', as it's just an API to private implementation details
   tests = filterAttrs (n: _: n != "debug") (import ./tests.nix);
+
+  derivationsIn = filterAttrs (_: isDerivation);
 };
 
 listToAttrs
   (map (system: {
          name  = system;
-         value = these system // { inherit tests; };
+         value = derivationsIn (these system // { inherit tests; });
        })
        [ "i686-linux" "x86_64-linux" ])
