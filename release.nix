9044d77ab751c737645d21f180ea7c266d53abc3
Chris Warburton <chriswarbo@gmail.com>
2017-05-11 18:23:34 +0100

    Do build both 32bit and 64bit, but use scopedImports to reduce boilerplate

--- release.nix
+++ release.nix
@@ -2,11 +2,29 @@ with builtins;
 with import <nixpkgs> {};
 with lib;
 with rec {
+  # Imports a Nix file, overriding currentSystem to be the given system
+  forSys = system:
+    let overrides = {
+          # Return the given system as if it were the current one
+          currentSystem = system;
+
+          # Force nested imports to use the given system too
+          import       = scopedImport overrides;
+          scopedImport = given: scopedImport (overrides // given);
+
+          # Update builtins.currentSystem (and builtins.builtins) too
+          builtins = builtins // overrides;
+        };
+     in scopedImport overrides;
+
   # Remove 'debug', as it's just an API to private implementation details
-  tests = mapAttrs (_: v: removeAttrs v [ "debug" ])
-                   (import ./tests.nix);
+  tests = system: mapAttrs (_: v: removeAttrs v [ "debug" ])
+                           (forSys system ./tests.nix);
 
   derivationsIn = filterAttrs (_: isDerivation);
+
+  defs = system: derivationsIn (forSys system ./. {});
 };
 
-derivationsIn (import ./. {}) // { inherit tests; }
+genAttrs [ "i686-linux" "x86_64-linux" ]
+         (system: defs system // { tests = tests system; })
