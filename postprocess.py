9cc5d3baf40e44069a60c9fb66c885382c74c3fe
Chris Warburton <chriswarbo@gmail.com>
2017-09-05 02:19:50 +0100

    Port postprocess to Python, use 'sorry' termination proof

---
+++ postprocess.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import json
+import os
+import re
+import sys
+
+# Read in our Isabelle input and split apart each definition
+
+def splitAt(word, s):
+    '''Takes a string 's' and splits it at all occurrences of the string 'word',
+    as long as 'word' appears at the start of a line (not the first line) and is
+    followed by whitespace.
+
+    Unlike the '.split' method, we don't strip 'word' from the results; it is
+    put back at the start of each string (except the first, since that doesn't
+    come from a split).'''
+
+    bits = re.split('\n' + word + '\s', s)
+    head = [] if bits == [] else [bits[0]]
+    tail = [word + ' ' + x for x in bits[1:]]
+
+    return head + tail
+
+def splitAll(word, strs):
+    '''Takes a list of strings 'strs' and applies 'splitAt(word, ...)' to each;
+    concatenating the results together.'''
+    return reduce(lambda result, s: result + splitAt(word, s),
+                  strs,
+                  [])
+
+def splitDefs(s):
+    '''Split a string of Isabelle apart into standalone definitions. The first
+    string in the resulting list is the preamble before the first definition and
+    the last entry will contain the end of the theory.'''
+    return reduce(lambda result, word: splitAll(word, result),
+                  ['end', 'function', 'fun', 'datatype'],
+                  [s])
+
+defs = splitDefs(sys.stdin.read())
+
+# Fix up the definitions we've found
+
+fixes = json.loads(open(os.getenv('FIXES'), 'r').read())
+
+def useSorry(d):
+    '''Replace TIP's choice of proofs with 'sorry'.'''
+    return d.replace('by pat_completeness auto', 'sorry\ntermination sorry')
+
+def defConstant(d):
+    '''Use 'definition' instead of 'fun' for names given in FIXES. See
+    http://stackoverflow.com/questions/28113667/why-must-isabelle-functions-have-at-least-one-argument/28147895
+    '''
+
+    # Ignore non-fun
+    if d[:3] != 'fun':
+        return d
+
+    # Extract the name and check if it's meant to use 'definition'
+    name = d.split()[1]
+    if name in fixes['constants']['encoded']:
+        return 'definition ' + d[3:]
+
+    # Otherwise return unchanged
+    return d
+
+def funToFunction(d):
+    '''Turns any given 'fun' definition into a 'function' definition.'''
+    if d.startswith('fun') and not d.startswith('function'):
+        pre  = 'function (sequential) '
+        post = '\nsorry\ntermination sorry\n'
+        return pre + d[3:] + post
+    return d
+
+print('\n'.join(map(funToFunction, map(defConstant, map(useSorry, defs)))))
