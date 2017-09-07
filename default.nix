30a08a560d02ebbfa3da4da9b0a7f41ba3aa32cd
Chris Warburton <chriswarbo@gmail.com>
2017-09-07 11:30:01 +0100

    Use newer nixpkgs Haskell library to build patched tip-lib

--- default.nix
+++ default.nix
@@ -140,6 +140,67 @@ rec {
     '';
   };
 
+  nixpkgs1703 =
+    with {
+      src = fetchFromGitHub {
+        owner  = "NixOS";
+        repo   = "nixpkgs";
+        rev    = "1360efe";
+        sha256 = "0y4db7akf7h03kpqr145qzzkbayamn9fkzxhx04abq5jdkxxjv0d";
+      };
+    };
+    import src { config = {}; };
+
+  tip-lib =
+    with rec {
+      withParser = runCommand "mk-parser"
+        {
+          buildInputs = [ bash haskellPackages.BNFC gcc ];
+          tipSrc      = fetchgit {
+            url    = http://chriswarbo.net/git/tip-tools.git;
+            rev    = "2b8ed14";
+            sha256 = "1nmja63agidjfspppgsrd6fv7fzc3vadw528cwj9vqajs9sgnla5";
+          };
+        }
+        ''
+          cp -r "$tipSrc" ./tip
+          chmod -R +w ./tip
+          pushd ./tip
+            bash make_parser.sh
+            ln -s $(gcc --print-file-name=libstdc++.so)
+            pushd tip-lib
+              ln -s $(gcc --print-file-name=libstdc++.so)
+            popd
+          popd
+          cp -r ./tip "$out"
+        '';
+
+      parserSrc = runCommand "tip-lib-with-parser"
+        {
+          inherit withParser;
+        }
+        ''
+          source $stdenv/setup
+
+          cp -a "$withParser/tip-lib" ./tip-lib
+          chmod +w -R ./tip-lib
+
+          for F in src/Tip/Pass/Pipeline.hs src/Tip/Passes.hs executable/Main.hs
+          do
+            echo "Patching $F" 1>&2
+            sed -e 's/\(import Options.Applicative\)/\1\nimport Data.Monoid ((<>))/' \
+                < "./tip-lib/$F" > temp
+            mv temp "./tip-lib/$F"
+          done
+
+          cp -a ./tip-lib "$out"
+        '';
+    };
+    nixpkgs1703.haskellPackages.haskellSrc2nix {
+      name = "tip-lib";
+      src  = parserSrc;
+    };
+
   isabelle-tip =
     with rec {
       te-benchmark-src = fetchgit {
@@ -152,15 +213,17 @@ rec {
       };
       haskellPackages = te-benchmark.patchedHaskellPackages;
     };
-    stdenv.mkDerivation {
-      name         = "isabelle-tip";
-      buildInputs  = [ (haskellPackages.ghcWithPackages (h: [ h.tip-lib ]))
-                       isaplanner jq perl ];
-      smtdata      = te-benchmark.tip-benchmark-smtlib;
-      FIXES        = ./fixes.json;
-      preprocess   = ./preprocess.sh;
-      postprocess  = ./postprocess.sh;
-      buildCommand = ''
+    runCommand "isabelle-tip"
+      {
+        buildInputs  = [ (haskellPackages.ghcWithPackages (h: [
+                           (h.callPackage tip-lib {}) ]))
+                         isaplanner jq perl ];
+        smtdata      = te-benchmark.tip-benchmark-smtlib;
+        FIXES        = ./fixes.json;
+        preprocess   = ./preprocess.sh;
+        postprocess  = ./postprocess.py;
+      }
+      ''
         source $stdenv/setup
         set -e
         set -o pipefail
@@ -169,7 +232,7 @@ rec {
         "$preprocess" < "$smtdata" | tip --isabelle | "$postprocess" > A.thy
 
         echo "Testing"
-        OUTPUT=$(echo 'use_thy "A";' | isaplanner)
+        OUTPUT=$(echo 'use_thy "A";' | isaplanner -o quick_and_dirty)
 
         if echo "$OUTPUT" |  grep -i -C 10 'error' > /dev/null
         then
@@ -183,7 +246,6 @@ rec {
         mkdir -p "$out"
         mv A.thy "$out/"
       '';
-    };
 
   haskellTypesOf = haskell-tip: runCommand "haskell-types-of"
     {
