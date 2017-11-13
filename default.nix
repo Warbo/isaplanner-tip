13f88814253b7772cdd5e2e5a1e81c4bfde3b818
Chris Warburton <chriswarbo@gmail.com>
2017-11-13 00:29:16 +0000

    Use haskell-te

--- default.nix
+++ default.nix
@@ -392,7 +392,17 @@ rec {
                            ])}/bin"
     '';
 
-  make_isacosy_theory = haskell-tip: runCommand "make_isacosy_theory"
+  haskell-te =
+    with {
+      src = fetchgit {
+        url    = http://chriswarbo.net/git/haskell-te.git;
+        rev    = "7a4cc07a";
+        sha256 = "04s9b60c6l2jji85ksmgab2gbxyddjxx4ps39rynjxr3fv1w550m";
+      };
+    };
+    import "${src}/nix-support" {};
+
+  make_isacosy_theory = runCommand "make_isacosy_theory"
     {
       buildInputs = [ makeWrapper ];
       raw         = writeScript "make_isacosy_theory-raw" ''
@@ -401,7 +411,8 @@ rec {
         cp "${isacosy-template}" "ISACOSY.thy"
 
         NAMES=$(cat)
-        TYPES=$(echo "$NAMES" | "${haskellTypesOf haskell-tip.annotatedAsts}" |
+        TYPES=$(echo "$NAMES" | "${haskellTypesOf
+                                     haskell-te.testData.tip-benchmark.asts}" |
                                 "${haskellToIsabelleTypes}")
         DATA='[]'
         while read -r PAIR
