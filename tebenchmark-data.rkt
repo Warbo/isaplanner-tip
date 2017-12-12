552cc3368a9d52c83006850f3079df1298e0ced1
Chris Warburton <chriswarbo@gmail.com>
2017-12-12 16:40:16 +0000

    Fixed extraction of function names and types from Isabelle
    Added a bunch of assertions, etc. to ensure we handle functions, constants and
    mutual recursion.

--- tebenchmark-data.rkt
+++ tebenchmark-data.rkt
@@ -4,12 +4,43 @@
 (require lib/util)
 (require lib/tip)
 (require json)
+(require racket/hash)
+(require racket/set)
+(require rackunit)
+(require syntax/macro-testing)
+
+;; FIXME: This is redundant once we're on newer tebenchmarks which exposes it
+(require/expose lib/tip (toplevel-function-names-in))
+
+;; Test data
+
+(define test-datatype
+  "datatype 'a T1 = C1 | C2 \"'a\"")
+
+(define test-definition
+  "definition x :: \"T1\" where \"f = g\"")
+
+(define test-function
+  "function f :: \"T1 => T2\" where \"f x = g x\"
+   sorry termination sorry")
+
+(define test-mutual
+  "function odds  :: \"'a List => 'a List\"
+        and evens :: \"'a List => 'a List\"
+   where \"odds  (nil      ) = nil              \"
+       | \"odds  (cons x xs) = cons x (evens xs)\"
+       | \"evens (nil      ) = nil              \"
+       | \"evens (cons x xs) = odds xs          \"
+   sorry termination sorry")
 
 ;; Data aquisition and transformation
 
 (define (assert-equal x y msg)
   (unless (equal? x y)
     (error msg x y)))
+
+(define (value->bool x)
+  (not (not x)))
 
 (define (strev s)
   (list->string (reverse (string->list s))))
@@ -42,36 +73,158 @@
 
 ;; List of Isabelle definitions (including datatypes and functions)
 (define isabelle-defs
-  (let* ((datatypes (split-at (list isabelle-defs-string) "datatype"))
-         (functions (split-at datatypes                   "function")))
-    (split-at functions "definition")))
+  (let* ((datatypes   (split-at (list isabelle-defs-string) "datatype"))
+         (functions   (split-at datatypes                   "function"))
+         (definitions (split-at functions                   "definition")))
+    ;; Filter out entries which only contain whitespace
+    (filter (lambda (def)
+              (not (empty? (string-split def))))
+            definitions)))
 
-;; (define (isabelle-type-of f)
-;;   #f)
+;; Returns the first double-quoted substring from the given string
+(define (first-quoted str)
+  (substring (second (split-at (list str) "\"")) 1))
 
+(assert-equal (first-quoted "fee fi \"fo fum\" tweedle dee \"tweedle dum\"")
+              "fo fum"
+              "first-quoted extracts as needed")
+
+;; The keyword used to begin an Isabelle definition
+(define (isabelle-keyword def)
+  (car (string-split def)))
+
+(assert-equal (isabelle-keyword test-function)
+              "function"
+              "Extracted Isabelle function keyword")
+
+(assert-equal (isabelle-keyword test-definition)
+              "definition"
+              "Extracted Isabelle definition keyword")
+
+(assert-equal (isabelle-keyword test-mutual)
+              "function"
+              "Extracted Isabelle function keyword from mutual recursion")
+
+(define (defines-isabelle-value? def)
+  (value->bool (member (isabelle-keyword def) (list "function" "definition"))))
+
+(assert-equal (defines-isabelle-value? test-function)
+              #t
+              "Isabelle functions are values")
+
+(assert-equal (defines-isabelle-value? test-definition)
+              #t
+              "Isabelle constants are values")
+
+(assert-equal (defines-isabelle-value? test-datatype)
+              #f
+              "Isabelle types are not values")
+
+;; We need to take mutual recursion into account
+(define (isabelle-types-of def)
+  (unless (defines-isabelle-value? def)
+    (error "Cannot get type of " def))
+  (map (lambda (part)
+         (string-normalize-spaces
+          (first-quoted (second (split-at (list part)
+                                          "::")))))
+       (split-at (list def) "and")))
+
+(assert-equal (isabelle-types-of test-function)
+              (list "T1 => T2")
+              "Got Isabelle function type")
+
+(assert-equal (isabelle-types-of test-definition)
+              (list "T1")
+              "Got Isabelle constant type")
+
+(assert-equal (isabelle-types-of test-mutual)
+              (list "'a List => 'a List"
+                    "'a List => 'a List")
+              "Got types of mutually recursive functions")
+
+;; We need to take mutual recursion into account
+(define (isabelle-names-of def)
+  (unless (defines-isabelle-value? def)
+    (error "Cannot get name of " def))
+  (map (lambda (part)
+         ;; Trim leading whitespace, in case any is left over from 'and'
+         (second (string-split (string-normalize-spaces part))))
+       (split-at (list def) "and")))
+
+(assert-equal (isabelle-names-of test-function)
+              (list "f")
+              "Can get function names")
+
+(assert-equal (isabelle-names-of test-definition)
+              (list "x")
+              "Can get constant names")
+
+(assert-equal (isabelle-names-of test-mutual)
+              (list "odds" "evens")
+              "Can get mutually recursive names")
+
+(define (isabelle-names-types-of def)
+  (make-immutable-hash (foldl (lambda (name type result)
+                                (cons (cons (string->symbol name) type)
+                                      result))
+                              '()
+                              (isabelle-names-of def)
+                              (isabelle-types-of def))))
+
+(assert-equal (isabelle-names-types-of test-definition)
+              (make-immutable-hash '((x . "T1")))
+              "Can get name/type mapping from constant")
+
+(assert-equal (isabelle-names-types-of test-function)
+              (make-immutable-hash '((f . "T1 => T2")))
+              "Can get name/type mapping from function")
+
+(assert-equal (isabelle-names-types-of test-mutual)
+              (make-immutable-hash '((odds  . "'a List => 'a List")
+                                     (evens . "'a List => 'a List")))
+              "Can get name/type mapping from mutually recursive functions")
 
 (define all-smt
   (read-benchmark (file->string (getenv "smtlib"))))
 
 (define all-function-names
-  (lowercase-names all-smt))
+  (append-map toplevel-function-names-in all-smt))
 
 ;; Some definitions are discarded since Isabelle doesn't understand them
 (define to-strip
   (let ((fixes (string->jsexpr (file->string (getenv "FIXES")))))
-    (append (hash-ref (hash-ref fixes 'nontypes)   'encoded)
-            (hash-ref (hash-ref fixes 'dependents) 'encoded))))
+    (map string->symbol
+         (append (hash-ref (hash-ref fixes 'nontypes)   'encoded)
+                 (hash-ref (hash-ref fixes 'dependents) 'encoded)))))
 
 (define remaining-functions
   (remove* to-strip all-function-names))
 
-;; (define function-types
-;;   (map (lambda (f) (tip-type-to-isabelle (type-of (def-of f))))
-;;        remaining-functions))
+(define isabelle-types-map
+  (foldl (lambda (def result)
+           (with-handlers
+               ;; Catch-all and report which def it was
+               ([(lambda (v) #t)
+                 (lambda (v)
+                   (error "Exception handling definition" def v))])
+             (if (defines-isabelle-value? def)
+                 (hash-union result (isabelle-names-types-of def))
+                 result)))
+         (make-immutable-hash '())
+         isabelle-defs))
+
+;; Sanity checks: definitions from Isabelle should match those of TIP
+
+(let ((from-tip      (list->set remaining-functions))
+      (from-isabelle (list->set (hash-keys isabelle-types-map))))
+  (unless (set=? from-tip from-isabelle)
+    (error "Mismatch between TIP and Isabelle function names"
+           `(missing-from-isabelle . ,(set-subtract from-tip from-isabelle))
+           `(extras-in-isabelle    . ,(set-subtract from-isabelle from-tip)))))
 
 (define data
-  `((function-names . ,(map symbol->string remaining-functions))
-    (isabelle-defs  . ,isabelle-defs)))
+  `((types . ,isabelle-types-map)))
 
 ;; Write out a JSON object, so jq can look up data for sampled names
 
