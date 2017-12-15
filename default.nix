1c3c108014350305409f5d68a7e9316d0cb9c999
Chris Warburton <chriswarbo@gmail.com>
2017-12-15 17:13:51 +0000

    Plumb new scripts into where they're needed in default.nix

--- default.nix
+++ default.nix
@@ -11,16 +11,20 @@ rec {
     haskell-te = callPackage ./haskell-te.nix {};
     isaplanner = callPackage ./isaplanner.nix {};
     isacosy    = callPackage ./isacosy.nix {
-      inherit (defs.isaplanner) isaplanner;
+      inherit (defs.isaplanner          ) isaplanner;
+      inherit (defs.scripts             ) extractEqs;
       inherit (defs.tebenchmark-isabelle) te-benchmark tebenchmark-data;
     };
     sampling = callPackage ./sampling.nix {
-      inherit (defs.haskell-te) get-haskell-te;
-      inherit (defs.isacosy) isacosy isacosy-theory;
+      inherit (defs.haskell-te          ) get-haskell-te;
+      inherit (defs.isacosy             ) isacosy isacosy-theory;
+      inherit (defs.scripts             ) isabelleTypeArgs;
       inherit (defs.tebenchmark-isabelle) tebenchmark-data tebenchmark-isabelle;
     };
+    scripts              = callPackage ./scripts {};
     tebenchmark-isabelle = callPackage ./tebenchmark-isabelle.nix {
       inherit (defs.isaplanner) isaplanner;
+      inherit (defs.scripts   ) getBenchmarkTypes getPreprocessed;
     };
   };
 }
