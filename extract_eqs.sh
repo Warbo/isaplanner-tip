574aaf7b40cf6a6f347cabb39d59bb3f0011099a
Chris Warburton <chriswarbo@gmail.com>
2017-12-15 00:42:59 +0000

    extraction changes

--- extract_eqs.sh
+++ extract_eqs.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+set -e
+set -o pipefail
 
+function check {
+    INPUT=$()
+    grep 'BEGIN OUTPUT' > /dev/null || {
+        echo "No 'BEGIN OUTPUT' sentinel found. Dumping whole output:" 1>&2
+        echo "$INPUT" 1>&2
+        exit 1
+    }
+}
+
+tee >(check)                                |
 tr -d '\n'                                  |
 grep -o -P '\[BEGIN OUTPUT,.*?END OUTPUT\]' |
 head -n1                                    |
