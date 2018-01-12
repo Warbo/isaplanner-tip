c5590097decdfadcd7370bda3df91df4d7797cd8
Chris Warburton <chriswarbo@gmail.com>
2018-01-12 15:32:11 +0000

    Benchmark env

--- benchmarks/default.nix
+++ benchmarks/default.nix
@@ -1,10 +1,38 @@
 with import ../pkgs.nix;
 with import ../. { pkgs = stable; };
+with defs.sampling;
 with rec {
-  py = stable.python.withPackages (p: [ p.subprocess32 ]);
+  python = stable.python.withPackages (pyPkgs: [ pyPkgs.subprocess32 ]);
+
+  rev     = "ce9c9478";
+
+  runners = mapAttrs (size: mapAttrs (rep: runner: rec {
+                       inherit runner;
+                       sample   = attrByPath [ rev size rep ] known-samples;
+                       analyser = sampleAnalyser {
+                         REP        = rep;
+                         SIZE       = size;
+                         # TODO: haskell-te commit e911a65 adds a SAMPLED_NAMES
+                         # argument, which lets us pass in our list of strings
+                         # without writing them to a file
+                         sampleFile = writeScript "sample-${rev}-${size}-${rep}"
+                                        (concatStringsSep "\n" sample);
+                       };
+                     }))
+                     (getAttr rev known-runners);
+
+  sampleAnalyser = stable.callPackage
+    "${defs.haskell-te.haskell-te-src}/benchmarks/sampleAnalyser.nix"
+    { inherit (defs.haskell-te.haskell-te) analysis; };
+
+  wrapper = stable.mkBin {
+    name  = "python";
+    paths = [ python ];
+    vars  = {
+      inherit runners;
+      timeout_secs = "180";
+    };
+    file  = "${python}/bin/python";
+  };
 };
-stable.mkBin {
-  name  = "python";
-  paths = [ py ];
-  file  = "${py}/bin/python";
-}
+stable.withDeps [ runner-tests ] python
