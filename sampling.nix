4379e89a2152e0498a1f8be68f5ae9e86dddc3f2
Chris Warburton <chriswarbo@gmail.com>
2018-01-30 18:19:12 +0000

    Multiple fixes to isacosy template values from samples

--- sampling.nix
+++ sampling.nix
@@ -80,166 +80,176 @@ rec {
     };
     args: withDeps [ filter-check ] (isacosy-from-sample-untested args);
 
-  isacosy-from-sample-untested = { label, names, teb ? te-benchmark }:
-    with rec {
-      namesFile = runCommand "filtered-names-${label}"
-        {
-          source = if isList names
-                      then writeScript "names-${label}"
-                                       (concatStringsSep "\n" names)
-                      else names;
-          buildInputs = [ jq ];
-          fixes       = ./scripts/fixes.json;
-        }
-        ''
-          set -e
-          cp "$source" tmp
+  dataForSample = { label, names, teb ? te-benchmark }: rec {
+    namesFile = runCommand "filtered-names-${label}"
+      {
+        source = if isList names
+                    then writeScript "names-${label}"
+                                     (concatStringsSep "\n" names)
+                    else names;
+        buildInputs = [ jq ];
+        fixes       = ./scripts/fixes.json;
+      }
+      ''
+        set -e
+        cp "$source" tmp
 
+        while read -r NAME
+        do
+          grep -v -x -F "$NAME" < tmp > tmp2
+          mv tmp2 tmp
+        done < <(jq -r '(.dependents.encoded +
+                         .nontypes.encoded) | .[]' < "$fixes")
+
+        mv tmp "$out"
+      '';
+
+    data    = make-tebenchmark-data     { te-benchmark = teb; };
+
+    theory  = make-tebenchmark-isabelle { te-benchmark = teb; };
+
+    allDefs = runCommand "all-defs"
+      {
+        inherit theory;
+        buildInputs = [ gawk jq ];
+      }
+      ''
+        set -e
+
+        # We have three conflicting criteria for listing definitions of
+        # sampled names:
+        #
+        #  - Going from a sample to their definitions needs to be reasonably
+        #    fast, since it will be executed a lot.
+        #  - We must output definitions in the same order as the names appear
+        #    in the sample, because they must match up with the 'functions'
+        #    list for zipping.
+        #  - Defs which use 'definition' will differ from those which use
+        #    'function'.
+        #
+        # We satisfy these by creating all defs once, up front, here. The name
+        # to definition process can then just traverse the names as given, and
+        # look up the definitions.
+
+        function getByKeyword {
+          # Awk command from https://unix.stackexchange.com/a/205854/63735
+          < "$theory" tr  '\n' ' '         | grep -o " $1 [^:]*" |
+                      sed -e "s/ $1 /\n/g" | awk '{$1=$1};1'     | grep '^.'
+        }
+
+        # All 'definitions' follow the keyword 'definition'
+        getByKeyword definition > defs
+
+        # Functions may follow 'function' or 'and' (for mutual-recursion)
+        getByKeyword function >  functions
+        echo ""               >> functions
+        getByKeyword 'and'    >> functions
+
+        grep '^.' < functions > funs
+
+        jq -R '{(.) : ("@{thms \"A." + . + "_def\"}")}' < defs |
+        jq -s 'reduce .[] as $obj ({}; . + $obj)'       > defs.json
+
+        jq -R '{(.) : ("@{thms \"A." + . + ".simps\"}")}' < funs |
+        jq -s 'reduce .[] as $obj ({}; . + $obj)'       > funs.json
+
+        jq -n --argfile d defs.json --argfile f funs.json '$d + $f' > "$out"
+      '';
+
+    datatypes = runCommand "datatypes-${label}"
+      {
+        inherit data isabelleTypeArgs namesFile;
+        buildInputs = [ fail jq ];
+        pre         = "ThyConstraintParams.add_datatype' @{context} @{typ \"";
+        post        = "\"}";
+      }
+      ''
+        set -e
+
+        # The type of each sampled name, e.g. 'nat => nat => nat' for plus
+        function completeTypes {
           while read -r NAME
           do
-            grep -v -x -F "$NAME" < tmp > tmp2
-            mv tmp2 tmp
-          done < <(jq -r '(.dependents.encoded +
-                           .nontypes.encoded) | .[]' < "$fixes")
+            jq -e --arg name "$NAME" '.types | has($name)' < "$data" \
+                                                           > /dev/null ||
+              fail "Couldn't get type of '$NAME'"
 
-          mv tmp "$out"
-        '';
-
-      data = make-tebenchmark-data { te-benchmark = teb; };
-
-      theory = make-tebenchmark-isabelle { te-benchmark = teb; };
-
-      datatypes = runCommand "datatypes-${label}"
-        {
-          inherit data isabelleTypeArgs namesFile;
-          buildInputs = [ fail jq ];
-          pre         = "ThyConstraintParams.add_datatype' @{context} @{typ \"";
-          post        = "\"}";
+            jq --arg name "$NAME" '.types | .[$name]' < "$data"
+          done < "$namesFile" | jq -s '.'
         }
-        ''
-          set -e
 
-          # The type of each sampled name, e.g. 'nat => nat => nat' for plus
-          function completeTypes {
-            while read -r NAME
-            do
-              jq -e --arg name "$NAME" '.types | has($name)' < "$data" \
-                                                             > /dev/null ||
-                fail "Couldn't get type of '$NAME'"
+        # Output arguments of a function type e.g. 'a => b => c' gives a and b
+        # We grep for 'global' to avoid standalone parameters like "'local1",
+        # e.g. from "cons :: 'local1 => 'local1 list => 'local1 list".
+        function allArgs {
+          completeTypes | "$isabelleTypeArgs" | jq -r '.[]' | grep -i global |
+                          sort -u
+        }
 
-              jq --arg name "$NAME" '.types | .[$name]' < "$data"
-            done < "$namesFile" | jq -s '.'
-          }
+        allArgs | while read -r ARG
+        do
+          echo "|> ThyConstraintParams.add_datatype' @{context} @{typ \"$ARG\"}"
+        done > "$out"
+      '';
 
-          # Output arguments of a function type e.g. 'a => b => c' gives a and b
-          # We grep for 'global' to avoid standalone parameters like "'local1",
-          # e.g. from "cons :: 'local1 => 'local1 list => 'local1 list".
-          function allArgs {
-            completeTypes | "$isabelleTypeArgs" | jq -r '.[]' | grep -i global |
-                            sort -u
-          }
+    definitions = runCommand "definitions-${label}"
+      {
+        inherit allDefs namesFile theory;
+        buildInputs = [ jq ];
+      }
+      ''
+        set -e
 
-          allArgs | while read -r ARG
+        function defs {
+          # Wrap up each sampled name in quotes
+          jq -R '.' < "$namesFile"
+        }
+
+        # Slurp each name into a list, look them up in allDefs, then join
+        defs | jq -rs --argfile defs "$allDefs" \
+                  'map($defs[.]) | join(", ")'  > "$out"
+      '';
+
+    functions = runCommand "functions-${label}"
+      {
+        inherit data namesFile;
+        buildInputs = [ fail jq ];
+      }
+      ''
+        set -e
+        set -o pipefail
+
+        function namesTypes {
+          # This assumes there are no spaces in the names, but that's a pretty
+          # safe assumption given that they're Isabelle identifiers, and (if
+          # they came from TIP) they'll be hex encoded as well.
+          while read -r NAME
           do
-            echo "|> ThyConstraintParams.add_datatype' @{context} @{typ \"$ARG\"}"
-          done > "$out"
-        '';
+            jq -e --arg name "$NAME" '.types | has($name)' < "$data" \
+                                                           > /dev/null ||
+              fail "Name '$NAME' is needed but wasn't found in '$data'"
 
-      definitions = runCommand "definitions-${label}"
-        {
-          inherit namesFile python theory;
-          commaSep = wrap {
-            name   = "commaSep";
-            paths  = [ python ];
-            script = ''
-              #!/usr/bin/env python
-              import sys
-              lines    = sys.stdin.readlines()
-              nonempty = [l.strip() for l in lines if l.strip() != ""]
-              print(', '.join(nonempty))
-            '';
-          };
+            TYPE=$(jq -r --arg name "$NAME" '.types | .[$name]' < "$data")
+
+            # We assume the definition comes from A.thy
+            echo "@{term \"A.$NAME :: $TYPE\"}" | jq -R '.'
+          done < "$namesFile"
         }
-        ''
-          set -e
 
-          function trimSpaces {
-            # From https://unix.stackexchange.com/a/205854/63735
-            awk '{$1=$1};1'
-          }
+        namesTypes | jq -rs 'join(", ")' > "$out"
+      '';
 
-          DEFS=$(grep -o '^definition[^:]*' < "$theory" |
-                 sed -e 's/^definition//g' | trimSpaces)
-
-          FUNS=$(grep -o '^function[^:]*' < "$theory" |
-                 sed -e 's/^function//g' | trimSpaces)
-
-          function onlyDefs {
-            grep -x -F -f <(echo "$DEFS") < "$namesFile"
-          }
-
-          function onlyFuns {
-            grep -x -F -f <(echo "$FUNS") < "$namesFile"
-          }
-
-          function isaDefs {
-            # Suffix each "definition" with '_def'
-            onlyDefs | awk '{printf("\n\n@{thms \"A.%s_def\"}\n\n", $0)}'
-          }
-
-          function isaFunctions {
-            # Suffix each "function" with '.simps'
-            onlyFuns | awk '{printf("\n\n@{thms \"A.%s.simps\"}\n\n", $0)}'
-          }
-
-          function isaAll {
-            isaDefs
-            isaFunctions
-          }
-
-          # Process each name on a separate line then combine with commas
-          isaAll | "$commaSep" > "$out"
-        '';
-
-      functions = runCommand "functions-${label}"
-        {
-          inherit data namesFile;
-          buildInputs = [ fail jq ];
-        }
-        ''
-          set -e
-          set -o pipefail
-
-          function namesTypes {
-            # This assumes there are no spaces in the names, but that's a pretty
-            # safe assumption given that they're Isabelle identifiers, and (if
-            # they came from TIP) they'll be hex encoded as well.
-            while read -r NAME
-            do
-              jq -e --arg name "$NAME" '.types | has($name)' < "$data" \
-                                                             > /dev/null ||
-                fail "Name '$NAME' is needed but wasn't found in '$data'"
-
-              TYPE=$(jq -r --arg name "$NAME" '.types | .[$name]' < "$data")
-
-              # We assume the definition comes from A.thy
-              echo "@{term \"A.$NAME :: $TYPE\"}" | jq -R '.'
-            done < "$namesFile"
-          }
-
-          namesTypes | jq -rs 'join(", ")' > "$out"
-        '';
-
-      undefined = stdenv.mkDerivation {
-        inherit namesFile;
-        name    = "undefined-${label}";
-        builder = listUndefined { te-benchmark = teb; };
-      };
+    undefined = stdenv.mkDerivation {
+      inherit namesFile;
+      name    = "undefined-${label}";
+      builder = listUndefined { te-benchmark = teb; };
     };
-    isacosy-theory {
-      inherit datatypes definitions functions undefined;
-      name = "sample-${label}";
-    };
+  };
+
+  isacosy-from-sample-untested = args: isacosy-theory {
+    inherit (dataForSample args) datatypes definitions functions undefined;
+    name = "sample-${args.label}";
+  };
 
   # The attribute names (e.g. ce9c9478) are the haskell-te revisions defining
   # the code which got benchmarked. The inner 'rev' values are the haskell-te
