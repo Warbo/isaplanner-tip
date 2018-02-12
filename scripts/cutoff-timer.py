c3753cf132cd61f0a4634ef637311221a05a4cdb
Chris Warburton <chriswarbo@gmail.com>
2018-02-12 01:00:08 +0000

    Make cutoff timer actually run processes

--- scripts/cutoff-timer.py
+++ scripts/cutoff-timer.py
@@ -2,8 +2,46 @@
 
 import json
 import os
+import signal
+import subprocess32
 import sys
 
 runners = json.loads(os.getenv('runners'))
 
-sys.stderr.write(repr(runners))
+timeout_secs = int(os.getenv('timeout_secs'))
+
+results = {}
+for size in runners:
+    results[size] = {}
+    for rep in runners[size]:
+        try:
+            sys.stderr.write('Running size {0} rep {1}\n'.format(size, rep))
+            proc     = subprocess32.Popen([runners[size][rep]],
+                                          stdout=subprocess32.PIPE,
+                                          stderr=subprocess32.PIPE,
+                                          preexec_fn=os.setsid)
+            out, err = proc.communicate(timeout=timeout_secs)
+            code     = proc.poll()
+            results[size][rep] = {
+                'stdout':    out,
+                'stderr':    err,
+                'timeout':   timeout_secs,
+                'timed out': False,
+                'error':     code != 0,
+                'exit code': code
+            }
+        except subprocess32.TimeoutExpired:
+            # Kill the process group, which will include all children
+            os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+
+            out, err = proc.communicate()
+            results[size][rep] = {
+                'stdout':    out,
+                'stderr':    err,
+                'timeout':   timeout_secs,
+                'timed out': True,
+                'error':     True,
+                'exit code': None
+            }
+
+print(json.dumps(results))
