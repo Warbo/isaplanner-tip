1f33d946bc56cc8fd6f34a590893d57fd77e6caf
Chris Warburton <chriswarbo@gmail.com>
2018-02-15 14:06:51 +0000

    Add failing test of lambdas in equations

--- scripts/eqsToJson.hs
+++ scripts/eqsToJson.hs
@@ -1,8 +1,9 @@
 #!/usr/bin/env runhaskell
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
 
 import           Control.Applicative           ((*>), (<*), (<|>))
 import           Data.Aeson
@@ -13,6 +14,7 @@ import           Data.List                     (elemIndex, intercalate, nub)
 import           Data.Maybe                    (fromJust)
 import           Numeric
 import           Prelude                       hiding (Eq)
+import qualified Prelude
 import           System.Environment            (lookupEnv)
 import           System.IO
 import           Test.QuickCheck
@@ -25,19 +27,15 @@ import           Text.ParserCombinators.Parsec hiding ((<|>))
 -- The parameter is how we index variables: we read them as strings, but output
 -- them as integers.
 
-data Eq   a = Eq (Expr a) (Expr a)
+data Eq   a = Eq (Expr a) (Expr a) deriving (Prelude.Eq)
 
 data Expr a = Const String | Var (Var a) | App (Expr a) (Expr a) | Lam (Expr a)
+     deriving (Prelude.Eq)
 
 data Var  a = Free a | Bound Int
+     deriving (Prelude.Eq, Show)
 
-varIndex (Free  i) = toJSON i
-varIndex (Bound i) = toJSON i
-
-isBound (Bound _) = True
-isBound (Free  _) = False
-
-instance ToJSON (Eq Int) where
+instance (ToJSON a) => ToJSON (Eq a) where
   toJSON (Eq lhs rhs) = object ["relation" .= String "~=",
                                 "lhs"      .= toJSON lhs ,
                                 "rhs"      .= toJSON rhs ]
@@ -55,12 +53,26 @@ instance ToJSON a => ToJSON (Expr a) where
                                    "lhs"    .= toJSON f            ,
                                    "rhs"    .= toJSON x            ])
 
+instance (ToJSON a) => Show (Eq a) where
+  show = B.unpack . encode
+
 -- Needed by 'notFollowedBy', for some reason
-instance Show (Expr String) where
+instance (ToJSON a) => Show (Expr a) where
   show = B.unpack . encode
 
 stringToEqs :: String -> [Eq Int]
 stringToEqs = map stringToEq . filter (/= "") . lines
+
+prop_stringToEqs =
+  stringToEqs (unlines [
+    "globalAA x = globalBB",
+    "",
+    "globalCC (globalDD ?x ?y) = globalEE ?y ?y"]) ===
+  [Eq (App (Const "globalAA") (Var (Free 0))) (Const "globalBB"),
+   Eq (App (Const "globalCC")
+           (App (App (Const "globalDD") (Var (Free 0)))
+                (Var (Free 1))))
+      (App (App (Const "globalEE") (Var (Free 1))) (Var (Free 1)))]
 
 stringToEq :: String -> Eq Int
 stringToEq s = case (parse parseExpr "lhs" lhs, parse parseExpr "rhs" rhs) of
@@ -70,6 +82,14 @@ stringToEq s = case (parse parseExpr "lhs" lhs, parse parseExpr "rhs" rhs) of
   where (lhs, rhs)   = (trim       (takeWhile (/= '=') s),
                         trim (tail (dropWhile (/= '=') s)))
         fail str err = error ("Couldn't parse '" ++ str ++ "': " ++ show err)
+
+prop_stringToEq_lambdas =
+  stringToEq "global64 (global65 (%a. ?a) Global6c) ?b = ?b" ===
+  (Eq (App (App (Const "global64") (App (App (Const "global65")
+                                             (Lam (Var (Bound 0))))
+                                        (Const "Global6c")))
+           (Var (Free 0)))
+      (Var (Free 0)))
 
 trim = reverse . trimLeading . reverse . trimLeading
   where trimLeading = dropWhile isSpace
@@ -112,6 +132,12 @@ numberExpr db e = case e of
   Const s        -> Const s
   Var   (Free s) -> Var (Free (fromJust (elemIndex s db)))
   App   f x      -> App (numberExpr db f) (numberExpr db x)
+
+varIndex (Free  i) = toJSON i
+varIndex (Bound i) = toJSON i
+
+isBound (Bound _) = True
+isBound (Free  _) = False
 
 -- Uses TemplateHaskell to look up definitions prefixed with 'prop_'
 return []
