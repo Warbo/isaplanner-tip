8085a234f1bb2a2486df2fd225e99abe67bf8158
Chris Warburton <chriswarbo@gmail.com>
2018-03-11 21:02:20 +0000

    Run rep 30

--- benchmarks/default.nix
+++ benchmarks/default.nix
@@ -11,7 +11,8 @@ with rec {
   # We originally ran only the even sizes (2, 4, 6, ..., 20), but turned out to
   # have enough time to run the odds too
   #rev    = "be30d74";  # Even numbered sample sizes
-  rev     = "3c15e23";  # Odd numbered sample sizes
+  #rev    = "3c15e23";  # Odd numbered sample sizes
+  rev     = "1c6ae74";  # Rep 30
 
   runners = mapAttrs (size: mapAttrs (rep: runner: rec {
                        inherit runner;
