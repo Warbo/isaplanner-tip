cafaeb10a22519c0ce048861e1d855a6424befe7
Chris Warburton <chriswarbo@gmail.com>
2018-03-12 07:31:19 +0000

    Bump haskell-te to non-dupe rep 30

--- haskell-te.nix
+++ haskell-te.nix
@@ -19,8 +19,8 @@ rec {
     "be30d74" = "0skmiy0riry3jxkz6pk3lf38bqd3lw6gxlh4l6j41ccdm7lwlh55";
     "fc310c1" = "1i3nkqxawnwjfarz96r2cxq0yrsw35cqbgxqi0a7n86arnc8da0k";
     "3c15e23" = "1y9ak8qwwa5g7ha2n2d26rbavgcv3y19s6qkk127wc0dip70z806";
-    "1c6ae74" = "0g8yhhvshav6hgdpkra3pz2avlyhh11rl1x56zakf370hawd78n2";
+    "68b98f8" = "1vb2crd6wcad21lmydr9ph3srk64v4l3p5qxyrymzqfxsx3c7sk9";
   };
 
-  inherit (get-haskell-te "1c6ae74") haskell-te haskell-te-src;
+  inherit (get-haskell-te "68b98f8") haskell-te haskell-te-src;
 }
